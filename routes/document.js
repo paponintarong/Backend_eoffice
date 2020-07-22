35e4276fd7d45dd8959d414c0e00eeff33d4e1fc
paponintarong <patiw12315@gmail.com>
2020-07-22 11:43:52 +0700

    Update document for add user

--- routes/document.js
+++ routes/document.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
-
+const ObjectID = require('mongodb').ObjectID;
 const DB_NAME = 'rtarf04';
 const TB_NAME = 'document'
 var mongo_db_url = "mongodb://" + DB_NAME + ":" + DB_NAME + "@122.155.202.161:27017/" + DB_NAME + "?authSource=" + DB_NAME;
@@ -52,5 +52,37 @@ router.post('/add-file', (req, res) => {
         }
     );
 });
+router.post('/update', (req, res) => {
+    let formData = req.body.formData;
+    let id = req.body.id;
 
+    connectMongoDBNoToken(res, async db => {
+        let dbo = db.db(DB_NAME);
+        const collection = dbo.collection(TB_NAME);
+
+        let query = {
+            "_id": ObjectID(id),
+        }
+
+        let updateCommand = {
+            $set: {
+                "users_action": formData.user_list
+            }
+        }
+
+        let result = await collection.updateOne(query, updateCommand);
+
+        db.close();
+        if (result) {
+            res.send({
+                status: true,
+                result
+            });
+        } else {
+            res.send({
+                status: false
+            });
+        }
+    });
+});
 module.exports = router;
